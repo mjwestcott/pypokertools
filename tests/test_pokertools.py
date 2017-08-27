f777e823c416c09c7daa23568ab5832709e3a8b7
Matt Westcott <m.westcott@gmail.com>
2017-08-27 20:39:45 +0100

    Add test_pokertools.py

---
+++ tests/test_pokertools.py
@@ -0,0 +1,30 @@
+from pokertools import (
+    CARDS,
+    CARD_NAMES,
+    HOLECARDS,
+    HOLECARDS_NAMES,
+)
+
+
+def test_pokertools():
+    assert len(CARDS) == len(CARD_NAMES) == 52
+    assert len(HOLECARDS) == len(HOLECARDS_NAMES) == 2652
+
+    seven_clubs = CARDS['7c']
+    assert seven_clubs.name == '7c'
+    assert seven_clubs.suit == 'c'
+    assert seven_clubs.rank == '7'
+    assert seven_clubs.numerical_rank == 7
+
+    ace_hearts = CARDS['Ah']
+    assert ace_hearts.name == 'Ah'
+    assert ace_hearts.suit == 'h'
+    assert ace_hearts.rank == 'A'
+    assert ace_hearts.numerical_rank == 14
+
+    assert CARDS['Ks'] > CARDS['Qh']
+    assert CARDS['2d'] <= CARDS['3d']
+
+    AhJh = HOLECARDS['Ah 7c']
+    assert AhJh[0] == ace_hearts
+    assert AhJh[1] == seven_clubs
